700913db9124c3308677aee2e62b0f46e9a2b1e6
Prachir <prachir1501@gmail.com>
2020-10-10 16:16:28 +0530

    footer year change

--- footer.php
+++ footer.php
@@ -29,7 +29,7 @@
     <div class="footer__copyright">
         <div class="row">
             <div class="col-2-of-3">
-                <p class="copyright">&copy; 2019 &dash; 2020, Council of Student Affairs, IIT Bhilai</p>
+                <p class="copyright">&copy; 2020 &dash; 2021, Council of Student Affairs, IIT Bhilai</p>
             </div>
             <div class="col-1-of-3"><p class="love">Made with <i class="fas fa-heart"></i></p></div>
         </div>
